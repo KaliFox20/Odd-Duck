51854f9a47c39c5438a07c6dd8809a84f50998ea
KaliFox20 <Daniel.Shiraishi@gmail.com>
2022-07-27 16:36:36 -1000

    finished lab12

--- app.js
+++ app.js
@@ -54,19 +54,24 @@ new Product('wine-glass');
 function randomIndexGenerator() {
   return Math.floor(Math.random() * allProducts.length);
 }
+
+let pIndexArr = [];
+
 //      make functunion to Generate random pictures non-repeating
 function renderImgs() {
-  let imgOneIndex = randomIndexGenerator();
-  let imgTwoIndex = randomIndexGenerator();
-  let imgThreeIndex = randomIndexGenerator();
-
 
 
   // make sure they are unique each round
-  while (imgOneIndex === imgTwoIndex || imgTwoIndex === imgThreeIndex || imgThreeIndex === imgOneIndex) {
-    imgTwoIndex = randomIndexGenerator();
-    imgThreeIndex = randomIndexGenerator();
+  while (pIndexArr.length < 6) {
+    let randNum = randomIndexGenerator();
+    if (!pIndexArr.includes(randNum)) {
+      pIndexArr.push(randNum);
+    }
   }
+  let imgOneIndex = pIndexArr.shift();
+  let imgTwoIndex = pIndexArr.shift();
+  let imgThreeIndex = pIndexArr.shift();
+
   imgOne.src = allProducts[imgOneIndex].photo;
   imgOne.alt = allProducts[imgOneIndex].name;
   allProducts[imgOneIndex].views++;
@@ -79,6 +84,8 @@ function renderImgs() {
 }
 
 renderImgs();
+
+
 //Event Handlers
 
 function handleClick(event) {
@@ -100,12 +107,98 @@ function handleClick(event) {
   }
 }
 
+// Chart Stuff
+
+let canvasElem = document.getElementById('my-chart');
+
+function renderChart() {
+  let pNames = [];
+  let pVotes = [];
+  let pViews = [];
+
+  for (let i = 0; i < allProducts.length; i++) {
+    pNames.push(allProducts[i].name);
+    pVotes.push(allProducts[i].votes);
+    pViews.push(allProducts[i].views);
+
+  }
+  let myObj = {
+    type: 'bar',
+    data: {
+      labels: pNames,
+      datasets: [{
+        label: '# of Votes',
+        data: pVotes,
+        backgroundColor: [
+          '#ff7300',
+          '#fffb00',
+          '#48ff00',
+          '#00ffd5',
+          '#002bff',
+          '#7a00ff',
+          '#ff00c8',
+          '#ff0000'
+        ],
+        borderColor: [
+          '#ff7300',
+          '#fffb00',
+          '#48ff00',
+          '#00ffd5',
+          '#002bff',
+          '#7a00ff',
+          '#ff00c8',
+          '#ff0000'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of Views',
+        data: pViews,
+        backgroundColor: [
+          '#ff0000',
+          '#ff7300',
+          '#fffb00',
+          '#48ff00',
+          '#00ffd5',
+          '#002bff',
+          '#7a00ff',
+          '#ff00c8',
+          '#ff0000'
+        ],
+        borderColor: [
+          '#ff0000',
+          '#ff7300',
+          '#fffb00',
+          '#48ff00',
+          '#00ffd5',
+          '#002bff',
+          '#7a00ff',
+          '#ff00c8',
+          '#ff0000'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+  new Chart(canvasElem, myObj);
+
+}
+
+
 function handleShowResults() {
   if (allVotes === 0) {
     for (let i = 0; i < allProducts.length; i++) {
-      let liElem = document.createElement('li');
-      liElem.textContent = `${allProducts[i].name}: views: ${allProducts[i].views}, votes: ${allProducts[i].votes}`;
-      resultList.appendChild(liElem);
+      renderChart();
+      // let liElem = document.createElement('li');
+      // liElem.textContent = `${allProducts[i].name}: views: ${allProducts[i].views}, votes: ${allProducts[i].votes}`;
+      // resultList.appendChild(liElem);
     }
     resultButton.removeEventListener('click', handleShowResults);
   }
